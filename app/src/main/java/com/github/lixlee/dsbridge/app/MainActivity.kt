62c48e0fdb0fbbc7842d6848f1efe9d5cd08b822
Lixlee <3918791+lixlee@users.noreply.github.com>
2023-02-25 09:56:14 +0800

    Demo App add WebView

--- app/src/main/java/com/github/lixlee/dsbridge/app/MainActivity.kt
+++ app/src/main/java/com/github/lixlee/dsbridge/app/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.github.lixlee.dsbridge.app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.lixlee.dsbridge.app.web.WebPageFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val webPageFragment = supportFragmentManager.findFragmentByTag("WebPageFragment") as? WebPageFragment
+        webPageFragment?.loadUrl("https://www.bing.com/")
     }
 }
---
+++ app/src/main/java/com/github/lixlee/dsbridge/app/web/WebPageFragment.kt
@@ -0,0 +1,130 @@
+package com.github.lixlee.dsbridge.app.web
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.net.http.SslError
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.*
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.github.lixlee.dsbridge.app.R
+import wendu.dsbridge.DWebView
+
+
+/*
+ * Created by lixlee on 2023/2/18
+ */
+open class WebPageFragment() : Fragment() {
+    private var webView: DWebView? = null
+    private var mUrl: String? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        val callback: OnBackPressedCallback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                handleOnBackPressedForWebView()
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_web_page, container, false)
+        webView = view.findViewById(R.id.web_view)
+        initWebView()
+        if (!mUrl.isNullOrEmpty()) {
+            loadUrl(mUrl)
+        }
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        webView?.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        webView?.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        webView?.destroy()
+        webView = null
+    }
+
+    fun loadUrl(url: String?) {
+        mUrl = url
+        if (url.isNullOrEmpty()) {
+            return
+        }
+        webView?.loadUrl(url)
+    }
+
+    private fun initWebView() {
+        DWebView.setWebContentsDebuggingEnabled(true)
+        webView?.webChromeClient = object : WebChromeClient() {
+            override fun onReceivedTitle(view: WebView?, title: String?) {
+                super.onReceivedTitle(view, title)
+                (activity as? AppCompatActivity)?.supportActionBar?.title = title
+            }
+        }
+        webView?.webViewClient = object : WebViewClient() {
+
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                return super.shouldOverrideUrlLoading(view, request)
+            }
+
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+            }
+
+            override fun shouldInterceptRequest(view: WebView?, request: WebResourceRequest?): WebResourceResponse? {
+                return super.shouldInterceptRequest(view, request)
+            }
+
+            override fun onReceivedError(view: WebView?, request: WebResourceRequest?, error: WebResourceError?) {
+                super.onReceivedError(view, request, error)
+            }
+
+            override fun onReceivedHttpError(view: WebView?, request: WebResourceRequest?, errorResponse: WebResourceResponse?) {
+                super.onReceivedHttpError(view, request, errorResponse)
+            }
+
+            override fun onReceivedSslError(view: WebView?, handler: SslErrorHandler?, error: SslError?) {
+                super.onReceivedSslError(view, handler, error)
+            }
+        }
+    }
+
+    private fun handleOnBackPressedForWebView() {
+        webView?.let {
+            if (it.canGoBack()) {
+                it.goBack()
+            } else {
+                requireActivity().finish()
+            }
+        }
+    }
+}
